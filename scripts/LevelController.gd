50eb2468b0bf0410dea4943bb97f7d980e177929
CaptainGobelin <captainGobelin@gmail.com>
2018-01-29 16:35:59 +0100

    fix scene 2 and 3

--- scripts/LevelController.gd
+++ scripts/LevelController.gd
@@ -5,3 +5,7 @@ export(float) var max_scale
 
 func _ready():
 	get_node("StreamPlayer").play(0)
+	get_node("../Label").text = ""
+	get_node("..").streamPlayer = get_node("StreamPlayer")
+	get_node("../Player").set_global_pos(get_node("StartPoint").get_global_pos())
+	get_node("../Player").path = null
