667cb20e68a8bae1218b4948b7245db1ea097259
CaptainGobelin <captainGobelin@gmail.com>
2018-01-29 10:44:04 +0100

    compress musics

--- scripts/LevelController.gd
+++ scripts/LevelController.gd
@@ -2,7 +2,6 @@ extends Node2D
 
 export(float) var min_scale
 export(float) var max_scale
-export(String) var music
 
 func _ready():
-	get_node("../SamplePlayer").play(music)
+	get_node("StreamPlayer").play(0)
